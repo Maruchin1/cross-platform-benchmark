e8f0e1c661362a5eae54eeb3eccbc089f7e755c6
Maruchin1 <marcinpk.mp@gmail.com>
2021-03-15 18:53:05 +0100

    ui-tester: setup project

---
+++ ui-tester/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "me.admin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
