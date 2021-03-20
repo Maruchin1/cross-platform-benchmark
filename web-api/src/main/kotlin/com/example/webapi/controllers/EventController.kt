fd77d9b82ae3419939e942795b48010369e3e9c9
Maruchin1 <marcinpk.mp@gmail.com>
2021-03-20 16:41:30 +0100

    web-api: enable cross origin

--- web-api/src/main/kotlin/com/example/webapi/controllers/EventController.kt
+++ web-api/src/main/kotlin/com/example/webapi/controllers/EventController.kt
@@ -2,6 +2,7 @@ package com.example.webapi.controllers
 
 import com.example.webapi.model.Event
 import com.example.webapi.service.EventService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,7 @@ class EventController(
     private val eventService: EventService
 ) {
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @GetMapping("/events/page")
     fun getPage(@RequestParam pageNumber: Int, @RequestParam pageSize: Int): List<Event> {
         return eventService.getPage(pageNumber, pageSize)
